50d061ff7c11a3b077c49042f7e1083f864bb919
Jan Kvapil <x408788@fi.muni.cz>
2025-12-12 12:21:52 +0100

    Build via Nix

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+    fenix,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = (import nixpkgs) {
+          inherit system;
+          overlays = [fenix.overlays.default];
+        };
+
+        meesign-crypto = pkgs.callPackage ./default.nix { };
+      in rec {
+        defaultPackage = meesign-crypto;
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            protobuf
+            pcsclite
+            pkg-config
+            rust-analyzer
+            (pkgs.fenix.stable.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+          ];
+        };
+      }
+    );
+}
